0a747804b91dfa4c533e26114c58f603498857f9
Alejandro <alejoatria@gmail.com>
2022-06-22 15:44:07 +0000

    realizado domain gen con 4 variables y 4 opciones cada una. 256 dominios.

--- src/app.js
+++ src/app.js
@@ -1,11 +1,30 @@
 /* eslint-disable */
+
 import "bootstrap";
 import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let pro = ["el", "nuestro", "su", "tu"];
+let adj = ["perfecto", "valiente", "hediondo", "horrible"];
+let suj = ["desconocido", "amante", "cocinero", "profesor"];
+let ext = [".com", ".io", ".net", ".us"];
+
 window.onload = function() {
-  //write your code here
+  for (let pron of pro) {
+    for (let adjet of adj) {
+      for (let sujet of suj) {
+        for (let exts of ext) {
+          let domain = [];
+          domain.push(pron);
+          domain.push(adjet);
+          domain.push(sujet);
+          domain.push(exts);
+          console.log(domain.join(""));
+        }
+      }
+    }
+  }
   console.log("Hello Rigo from the console!");
 };
